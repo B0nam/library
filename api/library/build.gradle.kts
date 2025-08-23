3eb8cf3e8338a4983469a5c13ae13d21aceac38c
daniel.rissardi <daniel.rissardi@db1.com.br>
2025-08-22 22:34:38 -0300

    chore: add database configurations with swagger and test container dependencies

--- api/library/build.gradle.kts
+++ api/library/build.gradle.kts
@@ -20,6 +20,10 @@ configurations {
 	}
 }
 
+val postgresVersion = "42.7.7"
+val testcontainersVersion = "1.21.3" // TODO: VERIFICAR VERSAO DO TESTCONTAINERS
+val springdocVersion = "2.8.4"
+
 repositories {
 	mavenCentral()
 }
@@ -27,14 +31,32 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+
 	implementation("org.liquibase:liquibase-core")
+	implementation("org.postgresql:postgresql:$postgresVersion")
+	implementation("org.testcontainers:postgresql:$testcontainersVersion")
+
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springdocVersion")
+
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.testcontainers:testcontainers:$testcontainersVersion") // TODO: VERIFICAR VERSAO DO TESTCONTAINERS
+	testImplementation("org.testcontainers:junit-jupiter:$testcontainersVersion") // TODO: VERIFICAR VERSAO DO TESTCONTAINERS
+	testImplementation("org.testcontainers:postgresql:$testcontainersVersion") // TODO: VERIFICAR VERSAO DO TESTCONTAINERS
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.register<Test>("integrationTest") {
+	description = "Executes the integration tests."
+	group = "verification"
+	useJUnitPlatform {
+		includeTags("integration")
+	}
 }
